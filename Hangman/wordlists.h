d28436c4f9d7a7d02e86a1366cabce233e5a50ee
Kartik Bhartiya <kartikbhartiya613@gmail.com>
2025-08-01 02:43:01 +0530

    Create wordlists.h

---
+++ Hangman/wordlists.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+std::string words[192] = {
+   "planet", "world", "friend", "musk", "bring", "lamp", "stone", "craft", "jumble", "fork",
+    "vixen", "paged", "blunt", "shirt", "clamp", "quest", "brick", "flame", "drown", "jump",
+    "sight", "broad", "chime", "plank", "glove", "drift", "shout", "brave", "climb", "vault",
+    "prank", "feast", "mild", "ghost", "quake", "wrist", "blend", "froth", "spice", "march",
+    "joust", "creak", "vigor", "plush", "batch", "dryer", "flick", "glint", "roast", "snip",
+    "badge", "chunk", "field", "graft", "hemp", "joint", "knurl", "lapse", "moist", "net",
+    "optic", "prawn", "quip", "rise", "scald", "tribe", "urge", "vamp", "whisk", "yacht",
+    "zinc", "bark", "dove", "elm", "fury", "grasp", "hint", "isco", "jog", "keg", "lop",
+    "maven", "nudge", "ox", "pant", "quip", "rhyme", "salt", "tiny", "undo", "vex", "waltz",
+    "xenon", "yelp", "zen", "blaze", "chop", "dusk", "envoy", "flicker", "grove", "hazy",
+    "imbue", "jab", "klutz", "limp", "mocha", "nifty", "opal", "pride", "quash", "robust",
+    "stump", "trick", "upend", "vow", "whirl", "xeric", "yield", "zebra", "abide", "bingo",
+    "crisp", "dwarf", "envoy", "flog", "glint", "hover", "intact", "joker", "knave", "lurch",
+    "mirth", "noble", "oaths", "plight", "quilt", "raven", "shove", "thick", "usher", "vigor",
+    "waltz", "xerox", "yodel", "zesty", "aspire", "blunt", "cider", "drank", "expose", "flush",
+    "grim", "haven", "icing", "jumpy", "knit", "leap", "mold", "newt", "ovary", "print",
+    "quack", "rash", "smelt", "tango", "usher", "vibe", "woven", "xerus", "yank", "zipper",
+    "apple", "budget", "carve", "dance", "echo", "faint", "gush", "hero", "icing", "juice",
+    "knot", "lamb", "maze", "nudge", "ocean", "pike", "quite", "rush", "slab", "time"
+};
+//add more words to wordlist from here
